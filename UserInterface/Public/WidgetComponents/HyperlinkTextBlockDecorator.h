0f74542e673cf8057debfedf6bca17b9f6971ad1
markoleptic <markalexcham@gmail.com>
2024-05-11 19:27:55 -0600

    Update FAQ for Audio Devices
    - Created FHyperlinkTextBlock and UHyperlinkTextBlock to allow for creating links within rich text.
    - Created SBSRichTextHyperlink to allow for changing the color of the text when hovered.
    - Updated the Audio Analyzer section of the FAQ and added a guide for how to use the Audio Device option as the Audio Source.

---
+++ UserInterface/Public/WidgetComponents/HyperlinkTextBlockDecorator.h
@@ -0,0 +1,43 @@
+﻿// Copyright 2022-2023 Markoleptic Games, SP. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/RichTextBlockDecorator.h"
+#include "HyperlinkTextBlockDecorator.generated.h"
+
+class UHyperlinkTextBlockDecorator;
+
+/** Underlying RichTextDecorator for UHyperlinkTextBlockDecorator. */
+class FHyperLinkTextBlockDecorator : public FRichTextDecorator
+{
+public:
+	FHyperLinkTextBlockDecorator(URichTextBlock* InOwner, UHyperlinkTextBlockDecorator* InDecorator);
+
+	virtual bool Supports(const FTextRunParseResults& RunParseResult, const FString& Text) const override;
+
+	virtual TSharedPtr<SWidget> CreateDecoratorWidget(const FTextRunInfo& RunInfo,
+		const FTextBlockStyle& DefaultTextStyle) const override;
+
+private:
+	void OnBrowserLinkClicked() const;
+
+	TDelegate<void()> OnHyperlinkClicked;
+	UHyperlinkTextBlockDecorator* Decorator;
+	mutable FString LinkText;
+};
+
+/** Opens RichText links in an external browser. */
+UCLASS()
+class USERINTERFACE_API UHyperlinkTextBlockDecorator : public URichTextBlockDecorator
+{
+	GENERATED_BODY()
+
+public:
+	UHyperlinkTextBlockDecorator(const FObjectInitializer& ObjectInitializer);
+
+	virtual TSharedPtr<ITextDecorator> CreateDecorator(URichTextBlock* InOwner) override;
+
+	UPROPERTY(EditAnywhere, Category = Appearance)
+	FHyperlinkStyle Style;
+};
